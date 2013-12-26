43c7ee775297b49706f57d89d8e1c0ed4d819d55
Aerotune <bue@Bues-MacBook-Air-2.local>
2013-12-26 13:39:41 +0100

    Empty rails template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Star::Application.config.secret_key_base = 'd2f56d5e9a7506f7477351bd36f3185526f91e9deb0751e15bc55696d345d43dc5b855429b37110b2a5cc7d979a3850a716f45d34af2f4e2933410fb06f3dd8a'
